885d4dad7f1651e4eaff599491d7f770f4c70811
Victor Ejike Nwosu <ejikello123@yahoo.com>
2024-05-17 12:21:00 +0200

    Add animal array

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ function greet(name) {
 }
 
 greet();
+
+export const animals = [
+  { id: 1, firstName: 'Lucia', type: 'Dog', accessory: 'House' },
+  { id: 2, firstName: 'Japa', type: 'Elephant', accessory: 'Car' },
+  { id: 3, firstName: 'Jeca', type: 'Cat', accessory: 'Bike' },
+  { id: 4, firstName: 'Markus', type: 'Lion', accessory: 'Pen' },
+  { id: 5, firstName: 'Jasper', type: 'Tiger', accessory: 'Car' },
+  { id: 6, firstName: 'klass', type: 'Goat', accessory: 'House' },
+];
