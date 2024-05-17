2d576072ce80928ddfeec958bfb860b44bfb9514
Victor Ejike Nwosu <ejikello123@yahoo.com>
2024-05-17 10:53:53 +0200

    Add grret function

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const greeting = 'Hello friend';
+
 console.log('Hello world!');
 console.log('Hell Everyone');
 console.log('Hello world!');
+
+function greet(name) {
+  console.log(greeting + name);
+}
+
+greet();
